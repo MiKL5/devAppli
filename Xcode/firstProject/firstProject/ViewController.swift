09a2ee1042d5ab3a501d1113fbe044d08b48860c
MiKL5 <miklgaillard@gmail.com>
2024-06-18 18:36:15 +0200

    🎉

---
+++ Xcode/firstProject/firstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstProject
+//
+//  Created by Mickael on 18/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world!")
+    }
+
+
+}
+
